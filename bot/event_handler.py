d92abd12458741ca54d86207c5e42a0fb68c106c
Vadim <vadim.skorba@gmail.com>
2016-05-18 19:07:32 +0200

    Update event_handler.py

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -87,8 +87,8 @@ class RtmEventHandler(object):
 
             if self.clients.is_bot_mention(msg_txt):
 
-                if re.search("^<@{}>[\s:]+all$".format(re.escape(self.clients.bot_user_id())), msg_txt):
-                    ch = self.msg_writer.find_channel_by_name(event['user'])
+                if re.search("^<@{}>[\s:]+alllll$".format(re.escape(self.clients.bot_user_id())), msg_txt):
+                    return self.msg_writer.send_message(event['channel'], json.dumps(event))
                     return self.msg_writer.send_message(ch, json.dumps(self._get_all(key)))
 
                 add_definition_regexp = re.search("^<@{}>[\s:]+(.+)\s*=\s*(.+)".format(re.escape(self.clients.bot_user_id())), msg_txt, re.MULTILINE|re.DOTALL)
